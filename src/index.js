41421abc601394cb0ed298a0255946e6cbb6a7ec
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-27 10:45:54 +1000

    toggle good doggos

--- src/index.js
+++ src/index.js
@@ -45,8 +45,56 @@ function renderDogInfo(pup){
     let h2 = document.createElement('h2');
     h2.innerText = pup.name;
     let button = document.createElement('button');
-
+    button.id = `btn${pup.id}`;
     button.innerText = pup.isGoodDog ? "Good Dog!" : "Bad Dog!";
 
+    let pupper = pup;
+    //listen to click event on button
+    button.addEventListener("click", toggleGoodDog);
+
     dogInfoContainer.append(img, h2, button);
+}
+
+function toggleGoodDog(event){
+    console.log(event.target.id);
+    btnId = event.target.id;
+    let button = document.querySelector("button#" + CSS.escape(btnId));
+    console.log(button);
+    let isGoodDog = true;
+    if (button.innerText === "Good Dog!"){
+        console.log("Good dogoo");
+        //update doggo
+        fetch(`${pupsBaseUrl}/${btnId.charAt(btnId.length-1)}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "isGoodDog": !isGoodDog
+            })
+        })
+            .then(res => res.json())
+            .then(obj => {
+                button.innerHTML = "Bad Dog!";
+            })
+    }
+    else {
+
+            //update doggo
+            fetch(`${pupsBaseUrl}/${btnId.charAt(btnId.length-1)}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "isGoodDog": isGoodDog
+                })
+        })
+            .then(res => res.json())
+            .then(obj => {
+                button.innerHTML = "Good Dog!";
+            })
+    }
 }
